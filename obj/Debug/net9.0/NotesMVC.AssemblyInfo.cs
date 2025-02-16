deced978586d2229a20186d3e8dcaa5bb2e2aac7
HQUJ <hristina.mecheva@gmail.com>
2025-02-16 20:50:21 +0200

    Geri-design2

--- obj/Debug/net9.0/NotesMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/NotesMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9c616ebaa507eb11f435f35177114f6feb7465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22900a345383692aaf522b19c5ccb5ce0152aead")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
