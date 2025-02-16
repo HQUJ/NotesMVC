a62b0606785477aff8d64c675e8339d355cb1f87
HQUJ <hristina.mecheva@gmail.com>
2025-02-16 16:36:11 +0200

    a

--- obj/Debug/net9.0/NotesMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/NotesMVC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cad76df306ee2a4e029cd17a6cf7cbb5b95390a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4712c1c604b9a41784ece8afe7d47105c8d8a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
